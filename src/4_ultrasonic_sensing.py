4af788d5ae8ddca2aa159febb98b0d80d1c537c5
pranaboy72 <86711384+pranaboy72@users.noreply.github.com>
2022-07-13 14:09:49 +0900

    Update 4_ultrasonic_sensing.py

--- src/4_ultrasonic_sensing.py
+++ src/4_ultrasonic_sensing.py
@@ -26,21 +26,83 @@ GPIO.output(TRIG1, False)
 GPIO.output(TRIG2, False)
 GPIO.output(TRIG3, False)
 GPIO.output(TRIG4, False)
-
-for i in range(4):
-  print(f"Waiting For Sensor {i+1} To Send Signal")
-  time.sleep(2)
-  GPIO.output(f"TRIG{i+1}", True) # if f_string doesn't work, type 1~4 Trig and Echo manually
-  time.sleep(0.00001)
-  GPIO.output(f"TRIG{i+1}", False)
-  print("Reading Sensor")
-  while GPIO.input(f"ECHO{i+1}")==0:
-    pulse_start = time.time()
-  while GPIO.input(f"ECHO{i+1}")==1:
-    pulse_end = time.time()
-  pulse_duration = pulse_end - pulse_start
-  distance = pulse_duration * 17150
-  distance = round(distance, 2)
-  print(f"Distance Sensor {i+1}: {distance}","cm")
-
-GPIO.cleanup()    #GPIO clean up: all designated GPIO numbers are cleaned up. Use this before you end up the code
+try:
+  i=1
+  while 1:
+    i=i%4
+    if i==1:
+      print(f"Waiting For Sensor {i} To Send Signal")
+      time.sleep(0.2)
+      GPIO.output(TRIG1, True) # if f_string doesn't work, type 1~4 Trig and Echo manually
+      time.sleep(0.00001)
+      GPIO.output(TRIG1, False)
+      print("Reading Sensor")
+      while GPIO.input(ECHO1)==0:
+        pulse_start = time.time()
+      while GPIO.input(ECHO1)==1:
+        pulse_end = time.time()
+      pulse_duration = pulse_end - pulse_start
+      distance = pulse_duration * 17150
+      distance = round(distance, 2)
+      print(f"Distance Sensor {i}: {distance}","cm")
+      time.sleep(2)
+      i+=1
+      
+   elif i==2:
+    print(f"Waiting For Sensor {i} To Send Signal")
+      time.sleep(0.2)
+      GPIO.output(TRIG2, True) # if f_string doesn't work, type 1~4 Trig and Echo manually
+      time.sleep(0.00001)
+      GPIO.output(TRIG2, False)
+      print("Reading Sensor")
+      while GPIO.input(ECHO2)==0:
+        pulse_start = time.time()
+      while GPIO.input(ECHO2)==1:
+        pulse_end = time.time()
+      pulse_duration = pulse_end - pulse_start
+      distance = pulse_duration * 17150
+      distance = round(distance, 2)
+      print(f"Distance Sensor {i}: {distance}","cm")
+      time.sleep(2)
+      i+=1
+      
+    elif i==3:
+    print(f"Waiting For Sensor {i} To Send Signal")
+      time.sleep(0.2)
+      GPIO.output(TRIG3, True) # if f_string doesn't work, type 1~4 Trig and Echo manually
+      time.sleep(0.00001)
+      GPIO.output(TRIG3, False)
+      print("Reading Sensor")
+      while GPIO.input(ECHO3)==0:
+        pulse_start = time.time()
+      while GPIO.input(ECHO3)==1:
+        pulse_end = time.time()
+      pulse_duration = pulse_end - pulse_start
+      distance = pulse_duration * 17150
+      distance = round(distance, 2)
+      print(f"Distance Sensor {i}: {distance}","cm")
+      time.sleep(2)
+      i+=1
+      
+    elif i==0:
+    print(f"Waiting For Sensor {i+4} To Send Signal")
+      time.sleep(0.2)
+      GPIO.output(TRIG4, True) # if f_string doesn't work, type 1~4 Trig and Echo manually
+      time.sleep(0.00001)
+      GPIO.output(TRIG4, False)
+      print("Reading Sensor")
+      while GPIO.input(ECHO4)==0:
+        pulse_start = time.time()
+      while GPIO.input(ECHO4)==1:
+        pulse_end = time.time()
+      pulse_duration = pulse_end - pulse_start
+      distance = pulse_duration * 17150
+      distance = round(distance, 2)
+      print(f"Distance Sensor {i+4}: {distance}","cm")
+      time.sleep(2) 
+      i+=1
+      
+      
+except KeyboardInterrupt:
+  print ("KeyboardInterrupt exception is caught")
+  GPIO.cleanup()    #GPIO clean up: all designated GPIO numbers are cleaned up. Use this before you end up the code
