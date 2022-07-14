eaf4784d47138bfb48fc3fcb8f40cafa2038488a
pranaboy72 <86711384+pranaboy72@users.noreply.github.com>
2022-07-14 12:11:20 +0900

    Update 4_ultrasonic_sensing.py

--- src/4_ultrasonic_sensing.py
+++ src/4_ultrasonic_sensing.py
@@ -22,84 +22,89 @@ GPIO.setup(TRIG3,GPIO.OUT)
 GPIO.setup(ECHO3,GPIO.IN)
 GPIO.setup(TRIG4,GPIO.OUT)
 GPIO.setup(ECHO4,GPIO.IN)
+
 GPIO.output(TRIG1, False)
 GPIO.output(TRIG2, False)
 GPIO.output(TRIG3, False)
 GPIO.output(TRIG4, False)
+
+print(f"Waiting For Sensor To Send Signal")
+time.sleep(2)
+
 try:
   i=1
   while 1:
     i=i%4
     if i==1:
-      print(f"Waiting For Sensor {i} To Send Signal")
-      time.sleep(0.2)
       GPIO.output(TRIG1, True) # if f_string doesn't work, type 1~4 Trig and Echo manually
       time.sleep(0.00001)
       GPIO.output(TRIG1, False)
-      print("Reading Sensor")
+      
+      print("Reading Sensor 1")
       while GPIO.input(ECHO1)==0:
         pulse_start = time.time()
       while GPIO.input(ECHO1)==1:
         pulse_end = time.time()
+        
       pulse_duration = pulse_end - pulse_start
-      distance = pulse_duration * 17150
+      distance = pulse_duration * 34300 / 2
       distance = round(distance, 2)
-      print(f"Distance Sensor {i}: {distance}","cm")
-      time.sleep(2)
+      print(f"Distance : {distance} cm")
+      time.sleep(0.4)
       i+=1
       
     elif i==2:
-      print(f"Waiting For Sensor {i} To Send Signal")
-      time.sleep(0.2)
       GPIO.output(TRIG2, True) # if f_string doesn't work, type 1~4 Trig and Echo manually
       time.sleep(0.00001)
       GPIO.output(TRIG2, False)
-      print("Reading Sensor")
+      
+      print("Reading Sensor2")
       while GPIO.input(ECHO2)==0:
         pulse_start = time.time()
       while GPIO.input(ECHO2)==1:
         pulse_end = time.time()
+        
       pulse_duration = pulse_end - pulse_start
-      distance = pulse_duration * 17150
+      distance = pulse_duration * 34300 / 2
       distance = round(distance, 2)
-      print(f"Distance Sensor {i}: {distance}","cm")
-      time.sleep(2)
+      print(f"Distance : {distance} cm")
+      time.sleep(0.4)
       i+=1
       
     elif i==3:
-      print(f"Waiting For Sensor {i} To Send Signal")
-      time.sleep(0.2)
       GPIO.output(TRIG3, True) # if f_string doesn't work, type 1~4 Trig and Echo manually
       time.sleep(0.00001)
       GPIO.output(TRIG3, False)
-      print("Reading Sensor")
+      
+      print("Reading Sensor3")
       while GPIO.input(ECHO3)==0:
         pulse_start = time.time()
       while GPIO.input(ECHO3)==1:
         pulse_end = time.time()
+        
       pulse_duration = pulse_end - pulse_start
-      distance = pulse_duration * 17150
+      distance = pulse_duration * 34300 / 2
       distance = round(distance, 2)
-      print(f"Distance Sensor {i}: {distance}","cm")
-      time.sleep(2)
+      print(f"Distance : {distance} cm")
+      time.sleep(0.4)
       i+=1
       
     elif i==0:
-      print(f"Waiting For Sensor {i+4} To Send Signal")
-      time.sleep(0.2)
       GPIO.output(TRIG4, True) # if f_string doesn't work, type 1~4 Trig and Echo manually
       time.sleep(0.00001)
       GPIO.output(TRIG4, False)
-      print("Reading Sensor")
+      
+      print("Reading Sensor4")
       while GPIO.input(ECHO4)==0:
         pulse_start = time.time()
       while GPIO.input(ECHO4)==1:
         pulse_end = time.time()
+        
       pulse_duration = pulse_end - pulse_start
-      distance = pulse_duration * 17150
+      distance = pulse_duration * 34300 / 2
       distance = round(distance, 2)
-      print(f"Distance Sensor {i+4}: {distance}","cm")
-      time.sleep(2) 
+      print(f"Distance : {distance} cm")
+      time.sleep(0.4)
       i+=1
       
       
