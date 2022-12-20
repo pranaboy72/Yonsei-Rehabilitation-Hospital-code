be7deea5a5108cbb7cf039f46a1c5ba103098d5a
pranaboy72 <86711384+pranaboy72@users.noreply.github.com>
2022-12-20 10:14:16 +0900

    Update posture.py

--- src/posture.py
+++ src/posture.py
@@ -6,30 +6,38 @@ import winsound as ws
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
-def calculate_dis(a,b):
+def calculate_neck(a,b): #shoulder, mouth
     a = np.array(a) # Shoulder
     b = np.array(b) # mouth
-    #print(a.type)
-    # radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
-    # angle = np.abs(radians*180.0/np.pi)
+
     distance = b[1]-a[1]
-    print(distance)
-    # if angle >180.0:
-    #     angle = 360-angle
+    #print(distance)
         
     return distance
 
-def beepsound():
+def calculate_back(a,b):    #shoulder, wrist #change a,b if the cam is caputring right of your body (default: left side)
+    a = np.array(a)
+    b = np.array(b)
+
+    radians = np.arctan2(a[1]-b[1], a[0]-b[0])
+    angle = abs(radians*180.0/np.pi)
+    #print(angle)
+    if angle > 180.0:
+        angle = 360-angle
+
+    return angle
+
+def beepsound1():
     freq = 2000
-    dur = 1000
+    dur = 100
     ws.Beep(freq, dur)
 
+def beepsound2():
+    freq = 500
+    dur = 100
+    ws.Beep(freq, dur)
 
-
-#path = "C:\\Users\\Junwoo.DESKTOP-0U9P69P\\Videos\\iVCam"
-#path = 'rtsp:/10.241.29.197/1/h264major'
-
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)   #To use ivcam, use cv2.VideoCapture(1)
 
 # Curl counter variables
 counter = 0 
@@ -56,13 +64,14 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
             landmarks = results.pose_landmarks.landmark
             
             # Get coordinates
-            shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+            shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
             mouth = [landmarks[mp_pose.PoseLandmark.MOUTH_LEFT.value].x, landmarks[mp_pose.PoseLandmark.MOUTH_LEFT.value].y]
             #elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
-            #wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
-            
+            wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+
             # Calculate angle
-            distance = abs(calculate_dis(shoulder, mouth))
+            distance = abs(calculate_neck(shoulder, mouth))
+            angle = abs(calculate_back(shoulder,wrist))
             
             # Visualize angle
             # cv2.putText(image, str(distance), 
@@ -71,13 +80,21 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
             #                     )
             
             # Curl counter logic
-            if distance > 0.14:
-                stage = "Okay"
+            if distance > 0.13:
+                stage1 = "Okay"
             else:
-                stage="Up!!!!!!"
-                print(beepsound())
+                stage1 ="Up!!"
+                print(beepsound1())
                 counter +=1
                 # print(counter)
+            
+            if angle < 85.0:
+                stage2 = "Okay" 
+                
+            else:
+                stage2 = "Up!!"
+                print(beepsound2())
+                counter+=1
                        
         except:
             pass
@@ -85,19 +102,25 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
         # Render curl counter
         # Setup status box
         cv2.rectangle(image, (0,0), (225,73), (245,117,16), -1)
+        cv2.rectangle(image, (0,73), (225,155), (245,117,16), -1)
         
         # Rep data
         # cv2.putText(image, 'Posture', (15,12), 
-        #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+        #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1,q cv2.LINE_AA)
         # cv2.putText(image, str(counter), 
         #             (10,60), 
         #             cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
         
         # # Stage data
-        cv2.putText(image, 'STAGE', (10,12), 
+        cv2.putText(image, 'HEAD', (10,12), 
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
-        cv2.putText(image, stage, 
+        cv2.putText(image, stage1, 
                     (10,60), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+        cv2.putText(image, 'BACK', (10,85), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+        cv2.putText(image, stage2, 
+                    (10,143), 
                     cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
         
         
